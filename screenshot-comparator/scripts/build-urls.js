f226a4e9a440a337cc6d4f5281caf2fe0ef6a61e
Vitali Taleika <vitalit@uniform.dev>
2025-07-01 17:31:48 +0400

    feat: screenshot urls from project map

---
+++ screenshot-comparator/scripts/build-urls.js
@@ -0,0 +1,88 @@
+import fs from "fs";
+import { ProjectMapClient } from "@uniformdev/project-map";
+import { RedirectClient } from "@uniformdev/redirect";
+import { env } from "./utils.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+try {
+  console.log("Reading entries from entries.json");
+  const entries = JSON.parse(fs.readFileSync("data/entries.json", "utf-8"));
+  console.log(`Found ${entries.length} entries.`);
+
+  const urls = new Set();
+
+  const projectMapClient = new ProjectMapClient({
+    apiKey: env("UNIFORM_API_KEY"),
+    projectId: env("UNIFORM_PROJECT_ID"),
+  });
+
+  const getStaticPaths = async () => {
+    const { nodes } = await projectMapClient.getNodes({});
+    return {
+      paths:
+        nodes?.filter((node) => node.compositionId).map((node) => node.path) ??
+        [],
+      fallback: false,
+    };
+  };
+
+  const nodePaths = await getStaticPaths();
+  const { paths } = nodePaths;
+  console.log(`Found ${paths.length} paths in project map.`);
+
+  const client = new RedirectClient({
+    apiKey: process.env.UNIFORM_API_KEY,
+    projectId: process.env.UNIFORM_PROJECT_ID,
+  });
+  const { redirects } = await client.getRedirects({ limit: 500 });
+
+  const updatedPaths = paths?.filter(
+    (path) =>
+      !redirects.some((redirect) => redirect.redirect.sourceUrl === path)
+  );
+  console.log(`Filtered paths: ${updatedPaths.length} valid paths.`);
+
+  const typeMapping = {
+    "articles": "ArticleDetail",
+    "conference": "ConferencesDetail",
+    "od-ls": "OnlineTrainingDetail",
+    "webinars": "WebinarsDetail",
+    "od-ls-watch-page": "OnlineTrainingDetail",
+    "hardGoodDetail": "HardGoodDetail",
+    "speaker": "PersonDetail"
+  }
+
+  updatedPaths.forEach((slug) => {
+    ///articles/:articles
+    if (slug.includes(":")) {
+      const spl = slug.split(":");
+      const entryType = spl[spl.length - 1].replace(/\/$/, "");
+      const typeFromMapping = typeMapping[entryType];
+      const entriesByType = entries.filter((e) => e.type === typeFromMapping);
+      console.log(`Found entries ${entriesByType.length} for type: ${entryType}`);
+      entriesByType.forEach((entry) => {
+        const url = new URL(
+          slug.replace(":" + entryType, entry.slug),
+          process.env.PROD_WEBSITE_URL
+        ).href;
+        urls.add(url);
+      });
+    } else {
+      const url = new URL(slug, process.env.PROD_WEBSITE_URL).href;
+      urls.add(url);
+    }
+  });
+
+  const a = Array.from(urls);
+  console.log(`Urls count: ${a.length}`);
+  a.sort();
+  fs.writeFileSync("./.temp/urls.json", JSON.stringify(a, null, 2), {
+    encoding: "utf8",
+  });
+
+  console.log("✅ build-urls complete! Check urls.json");
+} catch (error) {
+  console.error(`🆘 Error build-urls:`, error.message);
+  throw error;
+}
---
+++ screenshot-comparator/scripts/entries.js
@@ -0,0 +1,49 @@
+import { ContentClient } from '@uniformdev/canvas';
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+function env(key) {
+  const v = process.env[key];
+  if (!v) 
+  {
+      throw new Error("Add this env variable to .env file: " + key);
+  }
+  
+  return v;
+}
+
+if (fs.existsSync('data/entries.json')) {
+  console.log('entries.json exists, no need to download');
+}
+else
+{
+  const client = new ContentClient({
+    apiKey: env('UNIFORM_API_KEY'),
+    projectId: env('UNIFORM_PROJECT_ID'),
+  });
+
+  
+  const fetchVolume = 50; // 50 is max possible
+  let result = await client.getEntries({ limit: fetchVolume, skipPatternResolution: true });
+
+  let entries = result.entries;
+  console.log('got ' + entries.length + ' entries from server');
+  while (result.entries.length === fetchVolume) {
+    console.log('Checking if there are more on server with offset ' + entries.length);
+    result = await client.getEntries({
+      limit: fetchVolume, skipPatternResolution: true,
+      offset: entries.length
+    });
+
+    if (result.entries.length > 0) {
+      console.log('yep, got another '+ result.entries.length);
+      entries = [...entries, ...result.entries]
+    }
+  }
+    
+  console.log('Saving entries to entries.json');
+  try { fs.mkdirSync('data', { recursive: true }); } catch {}
+  fs.writeFileSync('data/entries.json', JSON.stringify(entries.map(x => ({ id: x.entry._id, name: x.entry._name.replace(/\n/g, ''), type: x.entry.type, slug: x.entry._slug })), null, 2), 'utf-8');
+  console.log('Saving completed');
+}
